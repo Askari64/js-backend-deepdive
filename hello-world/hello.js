fd3df60fb020ada3b94e949ac4229c6dcc5246da
Askari <askarir64@gmail.com>
2024-04-11 14:38:16 +0530

    modulated

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,6 +1,9 @@
-const add = require("./add");
+const maths = require("./maths");
 
 const name = "Askari";
 console.log(`Hello ${name}`);
 
-console.log(add(5, 6));
+console.log(maths.add(5,6));
+console.log(maths.sub(6,3));
+console.log(maths.multiply(6,8));
+console.log(maths.divide(2,7));
