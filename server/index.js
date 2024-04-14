ff416efebec9d0a069e605493a42fea65044ac51
Askari <askarir64@gmail.com>
2024-04-14 22:33:24 +0530

    express js initial

--- server/index.js
+++ server/index.js
@@ -1,35 +1,15 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()} : ${req.url} new Request Recieved \n`;
-  if (req.url === "/favicon.ico") return res.end();
-  const myUrl = url.parse(req.url, true);
-  console.log(myUrl);
-  fs.appendFile("./log.txt", log, (err, data) => {
-    switch (myUrl.pathname) {
-      case "/":
-        res.end("Home page");
-        break;
+const app = express();
 
-      case "/contact":
-        res.end("Contact Page");
-        break;
-
-      case "/about":
-        res.end("About Page");
-        break;
-
-      default:
-        res.end("404 not Found");
-        break;
-    }
-  });
-
-  console.log(req.headers);
+app.get("/", (req, res) => {
+  res.send("Hello from the server");
 });
 
-myServer.listen(8000, () => {
-  console.log("Server Started");
+app.get("/about", (req, res) => {
+  //using query parameters - http://localhost:8000/about?name=arg
+  res.send(`Hello ${req.query.name}.`);
 });
+
+app.listen(8000, () => console.log("🟢Server Started"));
