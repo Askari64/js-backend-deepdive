e753d1a8c2e982696fc3d4f9acee5c060a4a724f
Askari <askarir64@gmail.com>
2024-04-14 16:54:13 +0530

    formatted

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const http = require("http");
 const fs = require("fs");
-const url = require('url')
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   const log = `${Date.now()} : ${req.url} new Request Recieved \n`;
   if (req.url === "/favicon.ico") return res.end();
-  const myUrl = url.parse(req.url, true)
-  console.log(myUrl)
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("./log.txt", log, (err, data) => {
     switch (myUrl.pathname) {
       case "/":
